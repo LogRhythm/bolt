b1a0f65f7be9ef4cbfa99b8a97376565b615b2f0
Robert Weber <robert.weber@logrhythm.com>
2016-10-12 16:11:18 -0600

    fixing tests

--- cmd/bolt/main_test.go
+++ cmd/bolt/main_test.go
@@ -32,6 +32,7 @@ func TestStatsCommand_Run_EmptyDatabase(t *testing.T) {
 	}
 
 	db := MustOpen(0666, nil)
+	db.Compress = false
 	defer db.Close()
 	db.DB.Close()
 
@@ -72,6 +73,7 @@ func TestStatsCommand_Run(t *testing.T) {
 	}
 
 	db := MustOpen(0666, nil)
+	db.Compress = false
 	defer db.Close()
 
 	if err := db.Update(func(tx *bolt.Tx) error {
